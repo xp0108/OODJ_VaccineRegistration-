4a16bbf215127c8a07af705faa6c263d9ea4196f
xp <74288201+xp0108@users.noreply.github.com>
2021-11-26 23:31:21 +0800

    done manage centre

--- VaccineRegistrationSystem/src/vaccineregistrationsystem/ManageVaccine.java
+++ VaccineRegistrationSystem/src/vaccineregistrationsystem/ManageVaccine.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
